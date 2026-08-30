b635b595b7d3089fc2b8d60c024dd70775dce03d
waliddakhlaoui <waliddakhlaoui925@gmail.com>
2026-08-30 03:34:19 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,15 +1,24 @@
 import streamlit as st
 from prediction import predict_email
+
 st.title("--------SPAMGUARD--------")
-st.write("enter an email below and the model will classify it as spam or ham")
-email=st.text_area("email:",height=200,placeholder="paste your email here...")
-if st.button("classify email"):
-    if email.strip()=="":
-        st.warning ("please enter an email.")
+st.write("Enter an email below and the model will classify it as spam or ham")
+email = st.text_area(
+    "Email:",
+    height=200,
+    placeholder="Paste your email here...",
+    key="email_input"
+)
+if st.button("Classify email"):
+
+    if email.strip() == "":
+        st.warning("Please enter an email.")
     else:
-        prediction=predict_email(email)
-        if prediction=="SPAM":
-            st.error("SPAM")
+        prediction = predict_email(email)
+        if prediction == "SPAM":
+            st.error("🚨 SPAM")
         else:
-            st.success("HAM")
-            
+            st.success("✅ HAM")
+if st.button("Clear"):
+    st.session_state.email_input = ""
+    st.rerun()
